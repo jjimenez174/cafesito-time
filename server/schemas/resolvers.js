a93cd64cb90b36782a5419dc9827a8fbfba4d138
Jessy Castaneda <jessyc.12@icloud.com>
2024-06-10 23:15:52 -0700

    pushing code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
-const { User, Product, Category, Order } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User, Product, Category, Order } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 // const stripe = require('stripe')('');
 
 const resolvers = {
@@ -20,16 +20,16 @@ const resolvers = {
         };
       }
 
-      return await Product.find(params).populate('category');
+      return await Product.find(params).populate("category");
     },
     product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+      return await Product.findById(_id).populate("category");
     },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category',
+          path: "orders.products",
+          populate: "category",
         });
 
         user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
@@ -42,8 +42,8 @@ const resolvers = {
     order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category',
+          path: "orders.products",
+          populate: "category",
         });
 
         return user.orders.id(_id);
@@ -60,7 +60,7 @@ const resolvers = {
       for (const product of args.products) {
         line_items.push({
           price_data: {
-            currency: 'usd',
+            currency: "usd",
             product_data: {
               name: product.name,
               description: product.description,
@@ -72,9 +72,9 @@ const resolvers = {
       }
 
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
+        payment_method_types: ["card"],
         line_items,
-        mode: 'payment',
+        mode: "payment",
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`,
       });
@@ -114,10 +114,7 @@ const resolvers = {
     updateProduct: async (parent, { _id, quantity }) => {
       const decrement = Math.abs(quantity) * -1;
 
-      return await Product.findByIdAndUpdate(
-        _id,
-        { new: true }
-      );
+      return await Product.findByIdAndUpdate(_id, { new: true });
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -140,4 +137,3 @@ const resolvers = {
 };
 
 module.exports = resolvers;
-
